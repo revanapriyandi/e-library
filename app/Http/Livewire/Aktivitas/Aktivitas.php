33d059e8a888302f27a885ff91e76497f6ad126a
revanapriyandi <revanapriyandi19@gmail.com>
2020-11-23 21:23:40 +0700

    .

---
+++ app/Http/Livewire/Aktivitas/Aktivitas.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Aktivitas;
+
+use Livewire\Component;
+
+class Aktivitas extends Component
+{
+    public function render()
+    {
+        return view('aktivitas.aktivitas');
+    }
+}
